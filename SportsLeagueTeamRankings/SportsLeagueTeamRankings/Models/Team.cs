4c40640349982a7a8c37b28b21c0f2a1aa6515df
Tomo-Shortty <tomo_007@live.com.au>
2022-11-07 22:11:16 +1000

    Initial project commit

---
+++ SportsLeagueTeamRankings/SportsLeagueTeamRankings/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsLeagueTeamRankings.Models
+{
+    internal class Team
+    {
+        public string Name { get; set; }
+        public double Points { get; set; }
+    }
+}
